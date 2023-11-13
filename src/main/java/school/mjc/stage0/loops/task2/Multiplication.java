35b7000ae0ff758f1a16e47054c873732a312a71
Aleksei <alekseisligai@gmail.com>
2023-11-13 15:20:19 +0600

    Fixed Multiplication.java

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int count = 0;
-        while((multiplyByAndToInclusive > 0 && count <= multiplyByAndToInclusive) || (multiplyByAndToInclusive < 0 && count >= multiplyByAndToInclusive) ) {
+        while((multiplyByAndToInclusive > 0 && count <= multiplyByAndToInclusive) || (multiplyByAndToInclusive < 0 && count * (-1) >= multiplyByAndToInclusive) ) {
             System.out.println(count*multiplyByAndToInclusive);
-            count  = multiplyByAndToInclusive < 0? count-1:count+1;
+            count++;
         }
     }
 }
